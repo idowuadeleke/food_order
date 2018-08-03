5d2a1f8291fe871392b8218856894025e128e475
idowu adeleke <adelekeidowu30@gmail.com>
2018-08-03 15:52:16 +0100

    db.create issues

--- app.py
+++ app.py
@@ -31,13 +31,14 @@ api.add_resource(OrderByItemId, '/api/items/<int:item_id>/orders')
 api.add_resource(UserRegister, '/register')
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
 
